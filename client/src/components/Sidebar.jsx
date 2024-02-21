62827425fae8924384c8031832aa05e4c0a4f0a7
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-22 00:01:51 +0530

    Add login page, signup page and sidebar page for client side

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,12 +4,13 @@ import { FaHeart } from "react-icons/fa";
 import { MdOutlineExplore } from "react-icons/md";
 import { PiSignInBold } from "react-icons/pi";
 import { MdEditDocument } from "react-icons/md";
+import Logout from './Logout';
 
 const Sidebar = () => {
-  const authUser = false;
+  const authUser = true;
   return (
     <aside className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8
-    overflow-y-auto border-r bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10'>
+    overflow-y-auto border-r bg-glass'>
       
       <nav className='h-full flex flex-col gap-3'>
       <Link to='/' className='flex justify-center'>
