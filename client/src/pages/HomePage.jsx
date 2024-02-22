b335b5e40bdc580f65ca10dc09d986ecde317110
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-22 21:34:22 +0530

    Add Sort Repos functionality

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -54,15 +54,26 @@ const HomePage = () => {
 		setLoading(false);
 	};
 
+	const onSort = (sortType) => {
+		if (sortType === "recent") {
+			repos.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at)); //descending, recent first
+		} else if (sortType === "stars") {
+			repos.sort((a, b) => b.stargazers_count - a.stargazers_count); //descending, most stars first
+		} else if (sortType === "forks") {
+			repos.sort((a, b) => b.forks_count - a.forks_count); //descending, most forks first
+		}
+		setSortType(sortType);
+		setRepos([...repos]);
+	};
+
 	return (
 		<div className='m-4'>
-			<Search onSearch={onSearch}/>
-			<SortRepos />
+			<Search onSearch={onSearch} />
+			{repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
 			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
-			{userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
-            {!loading && <Repos repos={repos} />}
-
-			{loading && <Spinner />}
+				{userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+				{!loading && <Repos repos={repos} />}
+				{loading && <Spinner />}
 			</div>
 		</div>
 	);
