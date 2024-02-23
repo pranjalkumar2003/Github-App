731b83424f5a66797c1f9a775793ecb972e57ff8
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-23 21:03:55 +0530

    Add signup functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,8 +3,10 @@ import passport from 'passport';
 
 const router = express.Router();
 
-router.get("/github", passport.authenticate("github", { scope: ["user:email"] }),);
-router.get("/github/callback",
+router.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
+
+router.get(
+	"/github/callback",
 	passport.authenticate("github", { failureRedirect: process.env.CLIENT_BASE_URL + "/login" }),
 	function (req, res) {
 		res.redirect(process.env.CLIENT_BASE_URL);
