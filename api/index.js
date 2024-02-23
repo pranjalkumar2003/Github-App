080bb6597d74e6137e8f97f068572a6b24bc67e3
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-23 15:40:00 +0530

    Created a initial AuthRoute

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from 'cors';
+
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 import connectMongoDB from './db/connectMongoDB.js';
 
 dotenv.config();
@@ -14,6 +17,7 @@ app.get("/",(req, res) => {
     res.send({"message":"server is fetching the data"});
 });
 
+app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/explore",exploreRoutes);
 
